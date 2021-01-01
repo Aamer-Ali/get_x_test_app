5445c62b0b39aad5f0522d83e842bd4fbad36bef
Ajay Gokhale <ajay.g@callme.co.in>
2021-01-01 17:42:25 +0530

    Initial commit

---
+++ lib/controller/shopping_controller.dart
@@ -0,0 +1,26 @@
+import 'package:get/get.dart';
+import 'package:get_x_test_app/model/products.dart';
+
+class ShoppingController extends GetxController {
+  var products = List<Product>().obs;
+
+  @override
+  void onInit() {
+    // TODO: implement onInit
+    super.onInit();
+    fetchData();
+  }
+
+  void fetchData() {
+    var apiResult = [
+      Product(1, "Product 1", "Product 1 Image Url", "Product 1 Description",
+          25.50),
+      Product(2, "Product 2", "Product 2 Image Url", "Product 2 Description",
+          50.00),
+      Product(3, "Product 3", "Product 3 Image Url", "Product 3 Description",
+          15.75),
+    ];
+
+    products.assignAll(apiResult);
+  }
+}
---
+++ lib/model/products.dart
@@ -0,0 +1,10 @@
+class Product {
+  final int id;
+  final String productName;
+  final String productImgae;
+  final String productDescription;
+  final double price;
+
+  Product(this.id, this.productName, this.productImgae, this.productDescription,
+      this.price);
+}
